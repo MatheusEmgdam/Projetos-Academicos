0bbe2fa39270d935a46676572a15146f50ec4f5f
MatheusEmgdam <268079624+MatheusEmgdam@users.noreply.github.com>
2026-04-12 23:50:12 -0300

    modified:   Untitled-3.py

--- Untitled-3.py
+++ Untitled-3.py
@@ -7,19 +7,32 @@ def clear():
 
 excelente = 0 
 ruim = 0
+entrevistados = 10
 
-for i in range(10):
+for i in range(entrevistados):
     #INPUT
     nome=input("Digite o seu nome: ")
-    idade=int(input("Digite sua idade: "))
+    while True:
+        try: 
+            idade=int(input("Digite sua idade: "))
+            if 0>idade & idade<120:
+                break
+            else:
+                print("Digite uma idade entre 1 e 120 anos")
+        except ValueError:
+            print("Digite um número válido como idade")
     print("Como foi seu atendimento?\n")
     feedback=input("1-Excelente\n2-Bom\n3-Ruim\n")
     clear()
+    
     #Avaliação do Feedback
     match feedback.lower():
         case "1" | "excelente":
             excelente+=1
         case "3" | "ruim":
             ruim+=1
+        case _:
+            pass
+
 #OUTPUT
 print(f"Feedback de Avaliações:\n Excelentes:{excelente}\nRuins:{ruim}")
