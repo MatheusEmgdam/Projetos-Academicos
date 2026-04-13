4388382c0f2aefadee291a59259d82d521d787c5
MatheusEmgdam <268079624+MatheusEmgdam@users.noreply.github.com>
2026-04-12 22:24:17 -0300

    new file:   Untitled-3.py

---
+++ Untitled-3.py
@@ -0,0 +1,15 @@
+"""A empresa de marketing TudoWeb precisa realizar uma pesquisa de opinião com seus clientes para saber o grau de satisfação no atendimento. Sua tarefa é:
+
+    Desenvolver um programa em Python utilizando a estrutura de repetição FOR para coletar e exibir o retorno de uma pesquisa de atendimento ao cliente.
+    O programa deve solicitar a digitação do nome, idade e opinião do entrevistado sobre o atendimento prestado, sendo:
+        1: EXCELENTE
+        2: BOM
+        3: RUIM
+    A pesquisa deve ser feita com 50 entrevistados.
+    Ao final, o programa deverá exibir na tela:
+    a) Quantidade de respostas “EXCELENTE”
+    b) Quantidade de respostas “RUIM”
+    Utilize estruturas de decisão para verificar a opinião do entrevistado.
+    Realize testes com 10 entrevistados para validar o funcionamento do programa.
+    Compartilhe o projeto completo no seu repositório Github, informe o link do repositório no ambiente virtual.
+"""
